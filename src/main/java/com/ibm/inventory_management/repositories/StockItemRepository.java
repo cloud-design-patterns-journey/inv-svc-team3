81c3cbfa5fa0b0ff08e31bd72878bd794dde053b
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-05-27 10:11:57 +0200

    feat: use mongodb as backend

---
+++ src/main/java/com/ibm/inventory_management/repositories/StockItemRepository.java
@@ -0,0 +1,13 @@
+package com.ibm.inventory_management.repositories;
+
+import com.ibm.inventory_management.models.StockItem;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StockItemRepository extends MongoRepository<StockItem, String> {
+    Optional<StockItem> findById(String id);
+}
